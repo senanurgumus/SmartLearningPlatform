b5251bfcc1cabc8de66fc7ab7c46100a948a1f18
Senanur Gümüş <senanurgumus2003@outlook.com>
2025-04-12 15:52:10 +0300

    some edits

--- src/pages/MathUnitListPage.js
+++ src/pages/MathUnitListPage.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import mathQuiz from '../data/math_quiz.json';
 import { Link } from 'react-router-dom';
+import './MathUnitListPage.css';
+
 
 function MathUnitListPage() {
   const [units, setUnits] = useState([]);
@@ -10,15 +12,20 @@ function MathUnitListPage() {
   }, []);
 
   return (
-    <div>
-      <h2>Select a Math Unit</h2>
-      {units.map((unit) => (
-        <Link key={unit} to={`/module/math/quiz/${unit}`}>
-          <button style={{ margin: '10px' }}>{unit.replace(/_/g, ' ')}</button>
-        </Link>
-      ))}
+    <div className="math-unit-container">
+      <h2 className="math-unit-title">Select a Math Unit</h2>
+      <div className="unit-buttons-grid">
+        {units.map((unit) => (
+          <Link key={unit} to={`/module/math/quiz/${unit}`}>
+            <button className="unit-button">
+              {unit.replace(/_/g, ' ')}
+            </button>
+          </Link>
+        ))}
+      </div>
     </div>
   );
+  
 }
 
 export default MathUnitListPage;
